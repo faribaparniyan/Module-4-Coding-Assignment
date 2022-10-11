763d667f282da3e8b8c97c44fd980b8b1b31b504
faribaparniyan <faribaparniyan30@gmail.com>
2022-10-12 03:14:10 +0330

    style

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ names[9]="jim";
 
 for (var i = 0; i < names.length; i++) {
    if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
-    console.log("Goodbye" + names[i])
+    console.log("Goodbye " + names[i])
    }
-}
+   else {
+    console.log("Hello " + names[i])
+   }
+} 
